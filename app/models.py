f17ecba819b85498373fc6c5bb3b596a83ac7da0
@rustam_bulibekov <bulibekovr@gmail.com>
2022-08-10 18:13:15 +0200

    first commit

--- app/models.py
+++ app/models.py
@@ -1,18 +1,24 @@
-from datetime import  datetime
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-from app import db
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password_hash(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
 
 
 class Post(db.Model):
@@ -25,4 +31,6 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-a=  User(username="A", email="qwe@gmail.com")
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,22 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
+from app.models import User
 
 
-@app.route('/')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+#
+
 @app.route('/index')
 def index():
-    user = {'username': 'Эльдар Рязанов'}
+    user = {"username": "Rustam"}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,7 +31,9 @@ def index():
             'body': 'Какая гадость эта ваша заливная рыба!!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', user=user, title='Home', posts=posts)
+
+
 
 
 @app.route('/index1')
@@ -44,6 +56,28 @@ def index1():
     return render_template('index_1.html', title='Home', user=user, posts=posts)
 
 
+@app.route("/")
+@app.route('/index2')
+@login_required
+def index2():
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index2.html',  title='Home', posts=posts)
+
+
 @app.route('/login')
 def login():
     form = LoginForm()
@@ -56,7 +90,40 @@ def login1():
     if form.validate_on_submit():
         flash(f"Login requested for user {form.username.data}, {form.remember_me.data} ")
         return redirect(url_for('index'))
-    return render_template('/login1.html', title="Sign IN", form=form)
+    return render_template('login1.html', title="Sign IN", form=form)
+
+
+@app.route('/login2', methods=['GET', 'POST'])
+def login2():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login2'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login1.html', title='Sign In', form=form)
+
+
+@app.route('/login3', methods=['GET', 'POST'])
+def login3():
+    if current_user.is_authenticated:
+        return redirect(url_for('index2'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password_hash(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login3'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index2')
+        return redirect(next_page)
+    return render_template('login1.html', title='Sign In', form=form)
 
 
 """Операция, которая преобразует шаблон в полную HTML-страницу, называется рендерингом. Чтобы отобразить шаблон, 
